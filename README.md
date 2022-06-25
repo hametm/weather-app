b7db138b8fcd77c26436409992e26090212c633c
Morgan Hamet <morganhamet@gmail.com>
2022-06-25 14:51:13 -0700

    Add celsius/fahrenheit button and edit README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # weather-app
-Weather app project for The Odin Project. Photo credit to Freepik.
+Weather app project for The Odin Project. Photo credit to Freepik, materialdesignicons.com, and Flat Icon.
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
 import {decode} from 'html-entities';
 
 const input = document.querySelector("input");
-const submit = document.getElementById("submit");
+const search = document.getElementById("searchBtn");
 const name = document.getElementById("name");
 const temp = document.getElementById("temp");
 const feelsLike = document.getElementById("feels_like");
@@ -15,6 +15,9 @@ const low = document.getElementById("temp_min");
 const container = document.getElementById("weather");
 const page = document.querySelector("main");
 const errorMessage = document.createElement("h1");
+const convertToF = document.getElementById("fahrenheit");
+const convertToC = document.getElementById("celsius");
+const degreeSymbol = decode("&deg;");
 
 let location = "tokyo";
 
@@ -23,46 +26,71 @@ getWeather();
 
 input.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
-        submit.click();
+        search.click();
     }
 })
 
-submit.onclick = () => {
+search.onclick = () => {
     container.classList.remove("hidden");
     errorMessage.remove();
     location = input.value;
     getWeather();
 }
 
+
+
+
+
 async function getWeather() {
     try {
         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=0a10f192bc6dd2fa1798de607d95da55`, {mode: "cors"});
         const data = await response.json();
-        const degreeSymbol = decode("&deg;");
     
         name.textContent = data.name + convertCountryName(data.sys.country);
         temp.textContent = convertToFahrenheit(data.main.temp) + degreeSymbol;
         // feelsLike.textContent = `Feels like ${convertToFahrenheit(data.main.feels_like)}${degreeSymbol}`; 
         main.textContent = data.weather[0].main;
-        wind.textContent = `Wind: ${Math.round(data.wind.speed) + "mph"}`;
+        wind.textContent = `Wind: ${Math.round(data.wind.speed) + " mph"}`;
         high.textContent = `High: ${convertToFahrenheit(data.main.temp_max)}${degreeSymbol}`;
         low.textContent = `Low: ${convertToFahrenheit(data.main.temp_min)}${degreeSymbol}`;
+        changeTemp(temp, data.main.temp);
+        
     
     } catch(error) {
         container.classList.add("hidden");
-        errorMessage.textContent = "City not found :(";
+        errorMessage.textContent = "No such city exists!";
         errorMessage.classList.add("errorMessage");
         page.appendChild(errorMessage);
     }
    
 }
 
+function changeTemp(element, temp) {
+    convertToC.onclick = () => {
+        let newTemp = convertToCelsius(temp);
+        element.textContent = newTemp + degreeSymbol;
+    }
+
+    convertToF.onclick = () => {
+        let newTemp = convertToFahrenheit(temp);
+        element.textContent = newTemp + degreeSymbol;
+    }
+}
+
 function convertToFahrenheit(temp) {
+    convertToC.classList.remove("currentTemp");
+    convertToF.classList.add("currentTemp");
     return Math.round((((temp - 273)) * (9/5)) + 32);
 }
 
+function convertToCelsius(temp) {
+    convertToF.classList.remove("currentTemp");
+    convertToC.classList.add("currentTemp");
+    return Math.round(temp - 273);
+}
+
 function convertCountryName(name) {
-    if (name === "US") return "USA";
+    if (name === "US") return ", USA";
     if (name === "GB") return ", U.K.";
     return`, ${(countries.getName(name, "en"))}`;
 }
